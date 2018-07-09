6c71d1376babcb8e593f6e541fcfa1549109503a
Nels Salminen <n.salminen@me.com>
2018-07-09 03:52:05 +0200

    Added SVM classifier and completed training and test data processing

--- processFeatures.py
+++ processFeatures.py
@@ -3,18 +3,16 @@ import os.path
 import argparse
 import operator
 import csv
+from sklearn import svm
+from sklearn.preprocessing import normalize
+from sklearn.metrics import accuracy_score, classification_report
 from collections import OrderedDict
 import datetime
+import matplotlib.pyplot as plt
+import fnmatch
 
-def file_choices(choices, fname):
-    ext = os.path.splitext(fname)[1][1:]
-    if ext not in choices:
-       parser.error("File is not of type {}".format(choices))
-    return fname
-
-def process_file(file_path):
-    file_dict = np.load(file_path).item()
-    sorted_data = sorted(file_dict.items(), key=operator.itemgetter(0))
+def process_file(file_dict):
+    sorted_data = sorted(file_dict.items(), key = operator.itemgetter(0))
     return ([x[1] for x in sorted_data])
 
 def process_metadata(file_path, timestamps):
@@ -22,30 +20,73 @@ def process_metadata(file_path, timestamps):
     with open(file_path) as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
-            #print(row['UTC_timestamp'])
             start_time_object = datetime.datetime.strptime(row['UTC_timestamp'], "%Y-%m-%d %H:%M:%S")
             duration = int(row['duration'])
             for second in range(duration):
                 offset = datetime.timedelta(seconds=second)
                 current_timestamp = start_time_object + offset
                 date_time_index = datetime.datetime.strftime(current_timestamp, "%Y-%m-%dT%H:%M:%S")
-                metadata_dict[date_time_index] = 1
+                if date_time_index in metadata_dict:
+                    metadata_dict[date_time_index] = 1
 
     sorted_metadata = sorted(metadata_dict.items(), key=operator.itemgetter(0))
     return ([x[1] for x in sorted_metadata])
 
 parser = argparse.ArgumentParser(description='SVM classifier with parser for one or more feature arrays')
-parser.add_argument('metadata_file', type=str, help='File path to metadata file, with columns: duration, stream_timestamp, UTC_timestamp')
-parser.add_argument('feature_arrays', nargs='+', type=lambda s:file_choices(("npy", "npz"), s), help='Relative file paths of 1D feature arrays of equal length')
+parser.add_argument('training_metadata_file', type=str, help='Path to metadata file for training set, with columns: duration, stream_timestamp, UTC_timestamp')
+parser.add_argument('test_metadata_file', type=str, help='Path to metadata file for test set, with columns: duration, stream_timestamp, UTC_timestamp')
+parser.add_argument('training_dir', type=str, help='Path to directory only containing training feature dictionaries stored in .npy or .npz files')
+parser.add_argument('test_dir', type=str, help='Path to directory only containing test feature dictionaries stored in .npy or .npz files')
 args = parser.parse_args()
 
-first_dict = np.load(args.feature_arrays[0]).item()
-number_of_keys = len(first_dict)
-number_of_features = len(args.feature_arrays)
+training_feature_vector = number_of_keys = dict_keys = 0
+firstFile = True
+number_of_features = len(fnmatch.filter(os.listdir(args.training_dir),'*.npy'))
 
-feature_vector = X = np.empty(shape=[number_of_keys, number_of_features])
+for idx, val in enumerate(sorted(os.listdir(args.training_dir))):
+    if val.endswith(".npy"):
+        file_path = os.path.join(args.training_dir, val)
+        training_dict = np.load(file_path).item()
+        if firstFile:
+            number_of_keys = len(training_dict)
+            training_feature_vector = np.empty(shape=[number_of_keys, number_of_features])
+            firstFile = False
+            dict_keys = training_dict.keys()
+        training_feature_vector[:,idx-1] = process_file(training_dict)
+        continue
+    else:
+        continue
 
-for idx, val in enumerate(args.feature_arrays):
-    feature_vector[:,idx] = process_file(val)
+training_feature_vector = normalize(training_feature_vector, axis=0, norm='max')
+#plt.plot(training_feature_vector[:,3])
+#plt.show()
+training_labels = process_metadata(args.training_metadata_file, sorted(dict_keys))
+clf = svm.SVG(C=10)
+print('Training error:', clf.fit(training_feature_vector, training_labels).score(training_feature_vector, training_labels))
 
-labels = process_metadata(args.metadata_file, sorted(first_dict.keys()))
+test_feature_vector = number_of_keys = dict_keys = 0
+firstFile = True
+number_of_features = len(fnmatch.filter(os.listdir(args.test_dir),'*.npy'))
+
+for idx, val in enumerate(sorted(os.listdir(args.test_dir))):
+    if val.endswith(".npy"):
+        file_path = os.path.join(args.test_dir, val)
+        test_dict = np.load(file_path).item()
+        if firstFile:
+            number_of_keys = len(test_dict)
+            test_feature_vector = np.empty(shape=[number_of_keys, number_of_features])
+            firstFile = False
+            dict_keys = test_dict.keys()
+        processed = process_file(test_dict)
+        test_feature_vector[:,idx-1] = process_file(test_dict)
+        continue
+    else:
+        continue
+
+test_feature_vector = normalize(test_feature_vector, axis=0, norm='max')
+test_labels = process_metadata(args.test_metadata_file, sorted(dict_keys))
+clf.score(test_feature_vector, test_labels)
+predicted = clf.predict(test_feature_vector)
+#print(sorted(predicted))
+#print(accuracy_score(test_feature_vector, predicted))
+print(classification_report(test_labels, predicted))
